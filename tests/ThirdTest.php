e2e0ef3e0faefbd3e32c5c8f76c50b29699bf5ff
FoxxMD <FoxxMD@users.noreply.github.com>
2026-09-03 13:57:59 +0000

    remove debug statement

--- tests/ThirdTest.php
+++ tests/ThirdTest.php
@@ -46,7 +46,6 @@ class ThirdTest extends \PHPUnit\Framework\TestCase
 	    $this->assertEquals($this->power(3,0) , 1);
 	    $this->assertEquals($this->power(3,1) , 3);
         $this->assertEquals($this->power(3,4) , 81);
-        $this->assertEquals(number_format($this->power(3, 4), 4), "0.0123");
 
     }
 
